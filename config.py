e8436ab71912b6a3fafea5a4eabb4d6210223d22
anujedits42-byte <anujedits42@gmail.com>
2026-04-18 11:14:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+STRING_SESSION = os.getenv("STRING_SESSION")
+
+MAX_SIZE = 50 * 1024 * 1024
