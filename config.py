8dafd2116725260b257ff8f0505ddad376c1031b
anujedits42-byte <anujedits42@gmail.com>
2026-04-18 11:34:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,6 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 STRING_SESSION = os.getenv("STRING_SESSION")
 
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
 MAX_SIZE = 50 * 1024 * 1024
