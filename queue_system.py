df385678745041a7aa730f495b84c4b07783f51d
anujedits42-byte <anujedits42@gmail.com>
2026-04-18 11:53:02 +0530

    Update queue_system.py

--- queue_system.py
+++ queue_system.py
@@ -1,9 +1,19 @@
 import asyncio
 
 queue = asyncio.Queue()
+vip_queue = asyncio.Queue()
 
 async def worker():
     while True:
-        task = await queue.get()
-        await task()
-        queue.task_done()
+        if not vip_queue.empty():
+            task = await vip_queue.get()
+            await task()
+            vip_queue.task_done()
+        else:
+            task = await queue.get()
+            await task()
+            queue.task_done()
+
+async def start_workers():
+    for _ in range(7):
+        asyncio.create_task(worker())
